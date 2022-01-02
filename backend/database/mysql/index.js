34e1ba8c5d9c0a1601ed554ee6787f853bef5531
Matthew Olsen <mbolsen@gmail.com>
2022-01-02 13:05:23 -0700

    Done: fixed mysql connection issue

--- backend/database/mysql/index.js
+++ backend/database/mysql/index.js
@@ -1,10 +1,15 @@
+//start mysql in a terminal -> mysql -u <username> -p <password>
+//if you haven't already, then run the schema file.
+
+//need this line to use the process.env
+require('dotenv').config();
 var mysql = require('mysql2');
 // const API_URL = 'http://127.0.0.1:3000/classes';
 
 const dbConnection = mysql.createConnection({
   host: 'localhost',
-  user: 'root',
-  // password: '',
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
   database: 'chat',
 });
 
